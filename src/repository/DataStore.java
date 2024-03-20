4bc456ec88bd26f6b3314e8af4046ae1c5fc9252
sokpheng001 <kimchansokpheng123@gmail.com>
2024-03-20 13:48:44 +0700

    Updated: Encrypted data

--- src/repository/DataStore.java
+++ src/repository/DataStore.java
@@ -1,10 +1,16 @@
 package repository;
 
-import view.ApplicationView;
+import org.junit.Test;
 
-import java.awt.desktop.AppForegroundListener;
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 
+import java.security.Security;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -30,5 +36,42 @@ public class DataStore {
             System.out.println("Problem during read data from a file: " + exception.getMessage());
         }
         return stringStringMap;
+    }
+    @Test
+    public void writeData(){
+
+
+        String algorithms = "AES";
+//        key must be 18 bytes, 24 bytes, 32 bytes
+        String keyString = "Sokpheng123@*#$%";
+//
+        String data;
+
+        try(BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("dataStore/text.txt"));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("dataStore/words.txt"));
+        ){
+            try{
+//                SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(algorithms);
+
+                SecretKey secretKey = new SecretKeySpec(keyString.getBytes(),algorithms);
+                Cipher cipher = Cipher.getInstance(algorithms);
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+//
+                byte[] buffer = new byte[4096];
+                CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher);
+                while ((data=bufferedReader.readLine())!=null){
+
+                    for(int i=0;i<data.length();i++){
+                        cipherOutputStream.write(buffer, 0 ,data.charAt(i));
+                    }
+                }
+                cipherOutputStream.flush();
+                cipherOutputStream.close();
+            }catch (Exception exception){
+                System.out.println(STR."Error during encrypt data and write to file: \{exception.getMessage()}");
+            }
+        }catch (IOException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 }
